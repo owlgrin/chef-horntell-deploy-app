ce0617ba15213c9e32b01f9abd50659c23be055c
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-10-06 12:34:27 +0530

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'deploy-app'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures deploy-app'
+long_description 'Installs/Configures deploy-app'
+version          '0.1.0'
+
